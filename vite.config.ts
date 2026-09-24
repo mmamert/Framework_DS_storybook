227cf2da7213d3b096b6bee1dd4d889caca54d73
MatthieuM_SNCF <ext.matthieu.mamert@sncf.fr>
2026-09-24 11:07:11 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? "/Shadcn_DesignSystem/" : "/",
+  base: process.env.GITHUB_PAGES ? "/Framework_DS_storybook/" : "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
